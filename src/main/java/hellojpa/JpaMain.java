99dec0f6fda6c59d8b5791f13f51dc9e0e43e0ac
baesm971013 <bobbyb537@naver.com>
2025-01-03 22:58:09 +0900

    add column

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import jakarta.persistence.EntityManager;
 
 import java.util.List;
+import java.util.Map;
 
 public class JpaMain {
 
@@ -16,23 +17,8 @@ public class JpaMain {
         ts.begin();
         // process
         try {
-            // 비영속
-            Member member = new Member();
-            member.setId(51L);
-            member.setName("chat");
-
-            // 영속
-            System.out.println("-----Before-----");
-            em.persist(member);
-            System.out.println("-----After-----");
-
-            // 조회를 할떄 1차 cache에 저장
-            Member member1 = em.find(Member.class, 51L);
-            System.out.println("member1.getId() = " + member1.getId());
-            System.out.println("member1 = " + member1.getName());
-
-
             ts.commit();
+
         } catch (Exception e){
             System.out.println("exception");
             System.out.println(e);
--- src/main/java/hellojpa/Member.java
+++ src/main/java/hellojpa/Member.java
@@ -1,27 +1,55 @@
 package hellojpa;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.util.Date;
 
 @Entity
 public class Member {
     @Id
     private Long id;
+
+    @Column(name = "name", updatable = false, nullable = false)
     private String name;
 
-    public Long getId() {
-        return id;
-    }
+    private Integer age;
 
-    public void setId(Long id) {
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    @Lob
+    private String description;
+
+    @Transient
+    private int temp;
+
+    public Member(Long id, String name) {
         this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
         this.name = name;
     }
+    public Member(){
+
+    }
+
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
 }
