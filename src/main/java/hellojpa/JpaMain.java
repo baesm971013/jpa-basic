5872199ef59e0f710cdd02eee0666d048bdf16d3
baesm971013 <bobbyb537@naver.com>
2025-01-04 09:44:36 +0900

    feat: add sequence optimization

--- src/main/java/hellojpa/JpaMain.java
+++ src/main/java/hellojpa/JpaMain.java
@@ -17,9 +17,24 @@ public class JpaMain {
         ts.begin();
         // process
         try {
-            Member member = new Member();
-            member.setUserName("sumin");
-            em.persist(member);
+            Member member1 = new Member();
+            member1.setUserName("a");
+
+            Member member2 = new Member();
+            member2.setUserName("b");
+
+            Member member3 = new Member();
+            member3.setUserName("c");
+            System.out.println("--------------------------------");
+            em.persist(member1);
+            em.persist(member2);
+            em.persist(member3);
+
+            System.out.println(member1.getId());
+            System.out.println(member2.getId());
+            System.out.println(member3.getId());
+            System.out.println("--------------------------------");
+
             ts.commit();
 
         } catch (Exception e){
